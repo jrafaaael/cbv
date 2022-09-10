50e7031c8e38a5680fb9267d96c71b9dca993ec6
jrafaaael <jrmd070102@gmail.com>
2022-09-10 17:50:30 -0400

    create currencies class

---
+++ src/currencies.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class Currencies:
+    URL = "http://www.bcv.org.ve/"
+
+    def scrape(self):
+        page = requests.get(self.URL)
+        soup = bs(page.content, "html.parser")
+        dollar = soup.find("div", id="dolar")
+        currencies_parent = dollar.parent
+        currencies_wrapper = currencies_parent.find_all("div", recursive=False)
+        return currencies_wrapper
+
+    def parse(self):
+        container = self.scrape()
+        currencies = {}
+
+        for currency_wrapper in container:
+            code = currency_wrapper.find("span")
+            value = currency_wrapper.find("strong")
+
+            if code and value:
+                currencies[code.text.strip()] = value.text.strip()
+
+        return currencies
+
+    def get(self):
+        return self.parse()

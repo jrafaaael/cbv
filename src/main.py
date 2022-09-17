e569d3e3b6d678f8f1b6454cfcd1b3c86d3705ea
jrafaaael <jrmd070102@gmail.com>
2022-09-17 16:42:22 -0400

    add pytz to set Venezuela timezone in exchange updates

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import setup
 from currencies import get_currencies
 from database import db
+from pytz import timezone
+from datetime import datetime
+
 
 if __name__ == "__main__":
     currencies = get_currencies()
 
     for code, value in currencies:
+        now = datetime.now(timezone("America/Caracas")).isoformat()
+
         currency_id = (
             db.table("currency").select("id").eq("iso_4217", code).execute()
         ).data[0]["id"]
 
         response = (
             db.table("exchange")
-            .insert({"value": value, "symbol_id": currency_id})
+            .insert({"value": value, "symbol_id": currency_id, "created_at": now})
             .execute()
         )

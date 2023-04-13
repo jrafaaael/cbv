6e4292fc51cd12eefb268150aea7f09dc3905fd3
jrafaaael <jrmd070102@gmail.com>
2023-04-13 11:34:12 -0400

    fix decimal point in rate value

--- api/src/exchange/exchange.routes.ts
+++ api/src/exchange/exchange.routes.ts
@@ -3,14 +3,19 @@ import { Hono } from "hono";
 import { exchange as db } from "./exchange.database";
 
 const exchange = new Hono();
+const CONVERTION_OFFSET = 1000000;
 
 exchange.get("/", (c) => {
   const lastUpdatedData = db.map((exchange) => {
     const { rates, ...rest } = exchange;
+    const lastRate = rates.at(-1);
 
     return {
       ...rest,
-      rate: rates.at(-1),
+      rate: {
+        ...lastRate,
+        value: (lastRate?.value ?? 0) / CONVERTION_OFFSET,
+      },
     };
   });
 

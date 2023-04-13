8905657da869650dda2d46f3582c89b9ebc212fb
jrafaaael <jrmd070102@gmail.com>
2023-04-13 11:59:07 -0400

    add route to get currency specific data

--- api/src/exchange/exchange.routes.ts
+++ api/src/exchange/exchange.routes.ts
@@ -5,21 +5,45 @@ import { exchange as db } from "./exchange.database";
 const exchange = new Hono();
 const CONVERTION_OFFSET = 1000000;
 
-exchange.get("/", (c) => {
-  const lastUpdatedData = db.map((exchange) => {
-    const { rates, ...rest } = exchange;
+exchange
+  .get("/", (c) => {
+    const lastUpdatedData = db.map((exchange) => {
+      const { rates, ...rest } = exchange;
+      const lastRate = rates.at(-1);
+
+      return {
+        ...rest,
+        rate: {
+          ...lastRate,
+          value: (lastRate?.value ?? 0) / CONVERTION_OFFSET,
+        },
+      };
+    });
+
+    return c.json(lastUpdatedData);
+  })
+  .get("/:currencyNameOrId", (c) => {
+    const currencyNameOrId = c.req.param("currencyNameOrId");
+    const currency = db.find(
+      (exchange) =>
+        exchange.id.toLowerCase() === currencyNameOrId.toLowerCase() ||
+        exchange.name.toLowerCase() === currencyNameOrId.toLowerCase()
+    );
+
+    if (!currency) {
+      return c.json({ message: "Currency not found" }, 404);
+    }
+
+    const { rates, ...rest } = currency;
     const lastRate = rates.at(-1);
 
-    return {
+    return c.json({
       ...rest,
       rate: {
         ...lastRate,
         value: (lastRate?.value ?? 0) / CONVERTION_OFFSET,
       },
-    };
+    });
   });
-
-  return c.json(lastUpdatedData);
-});
 
 export { exchange };
